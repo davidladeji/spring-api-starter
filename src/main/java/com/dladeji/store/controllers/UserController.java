64e5a2a8392b7dbf16c1e843403f809aa856f806
davidladeji <71533464+davidladeji@users.noreply.github.com>
2025-05-13 16:50:09 -0700

    Add Delete resource endpoint

--- src/main/java/com/dladeji/store/controllers/UserController.java
+++ src/main/java/com/dladeji/store/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -82,5 +83,16 @@ public class UserController {
 
         var userDto = userMapper.toDto(user);
         return ResponseEntity.ok(userDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        var user = userRepository.findById(id).orElse(null);
+
+        if (user == null)
+            return ResponseEntity.notFound().build();
+
+        userRepository.delete(user);
+        return ResponseEntity.noContent().build();
     }
 }
