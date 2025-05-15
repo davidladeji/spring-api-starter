e4a193d58b7632e6445ee47d064ef154e300c4e0
davidladeji <71533464+davidladeji@users.noreply.github.com>
2025-05-15 14:47:34 -0700

    Capstone: Created entity objects for new tables

---
+++ src/main/java/com/dladeji/store/entities/Cart.java
@@ -0,0 +1,29 @@
+package com.dladeji.store.entities;
+
+import java.sql.Date;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "carts")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "date_created")
+    private Date dateCreated;
+}
