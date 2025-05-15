38a221c068547ae2501f504813063526e5cd8356
davidladeji <71533464+davidladeji@users.noreply.github.com>
2025-05-15 14:32:53 -0700

    Capstone: added new sql tables

---
+++ src/main/resources/db/migration/V2__create_cart_and_cartitem_tables.sql
@@ -0,0 +1,18 @@
+create table carts (
+    id              binary(16) default (uuid_to_bin(uuid())) NOT NULL,
+    date_created    DATE Not NULL,
+    PRIMARY KEY (id)
+);
+
+create table cart_items (
+    id          BIGINT AUTO_INCREMENT NOT NULL,
+    quantity    INTEGER NOT NULL,
+    product_id  BIGINT NOT NULL,
+    cart_id     binary(16) NOT NULL,
+    PRIMARY KEY (id),
+    Constraint items_product_unique UNIQUE(cart_id, product_id),
+    Constraint items_products_id_fk 
+        FOREIGN Key (product_id) REFERENCES products(id) ON DELETE CASCADE,
+    Constraint items_carts_id_fk 
+        FOREIGN Key (cart_id) REFERENCES carts(id) ON DELETE CASCADE
+);
