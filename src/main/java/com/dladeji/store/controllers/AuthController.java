cd3798cb95e0371eb8dc82607646d789f063bca3
davidladeji <71533464+davidladeji@users.noreply.github.com>
2025-05-26 21:14:15 -0700

    Generating Web Tokens
    This commit runs into a WeakKeyException since the key of our token ("secret") is too short

--- src/main/java/com/dladeji/store/controllers/AuthController.java
+++ src/main/java/com/dladeji/store/controllers/AuthController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dladeji.store.dtos.JwtResponse;
 import com.dladeji.store.dtos.LoginUserDto;
+import com.dladeji.store.services.JwtService;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -21,16 +23,18 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/auth")
 public class AuthController {
     private AuthenticationManager authenticationManager;
+    private JwtService jwtService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(
+    public ResponseEntity<JwtResponse> login(
             @Valid @RequestBody LoginUserDto request) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         request.getEmail(),
                         request.getPassword()));
 
-        return ResponseEntity.ok().build();
+        var token = jwtService.generateToken(request.getEmail());
+        return ResponseEntity.ok(new JwtResponse(token));
     }
 
     @ExceptionHandler(BadCredentialsException.class)
