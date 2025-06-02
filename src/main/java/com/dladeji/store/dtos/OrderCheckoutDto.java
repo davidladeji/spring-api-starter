5d7e50a34951a56d2ca81828980ab68b9994f36e
davidladeji <71533464+davidladeji@users.noreply.github.com>
2025-06-02 14:49:05 -0700

    Creating a checkout session

--- src/main/java/com/dladeji/store/dtos/OrderCheckoutDto.java
+++ src/main/java/com/dladeji/store/dtos/OrderCheckoutDto.java
@@ -7,4 +7,5 @@ import lombok.Data;
 @AllArgsConstructor
 public class OrderCheckoutDto {
     private Long orderId;
+    private String checkoutUrl;
 }
--- src/main/java/com/dladeji/store/services/CheckoutService.java
+++ src/main/java/com/dladeji/store/services/CheckoutService.java
@@ -2,23 +2,30 @@ package com.dladeji.store.services;
 
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.dladeji.store.dtos.OrderCheckoutDto;
 import com.dladeji.store.entities.Order;
 import com.dladeji.store.exceptions.CartIsEmptyException;
 import com.dladeji.store.repositories.OrderRepository;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CheckoutService {
-    private CartService cartService;
-    private AuthService authService;
-    private OrderRepository orderRepository;
+    private final CartService cartService;
+    private final AuthService authService;
+    private final OrderRepository orderRepository;
+
+    @Value("")
+    private String websiteUrl;
     
-    public OrderCheckoutDto checkout(UUID cartId){
+    public OrderCheckoutDto checkout(UUID cartId) throws StripeException{
         var cart = cartService.getCartObj(cartId);
 
         if (cart.getItems().isEmpty())
@@ -28,8 +35,36 @@ public class CheckoutService {
         
         var order = Order.fromCart(cart, user);
         orderRepository.save(order);
+
+        // Create a checkout session
+        var builder = SessionCreateParams.builder()
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
+                .setCancelUrl(websiteUrl + "/checkout-cancel");
+
+        order.getItems().forEach(item -> {
+            var lineItem = SessionCreateParams.LineItem.builder()
+                .setQuantity(Long.valueOf(item.getQuantity()))
+                .setPriceData(
+                    SessionCreateParams.LineItem.PriceData.builder()
+                        .setCurrency("usd")
+                        .setUnitAmountDecimal(item.getUnitPrice())
+                        .setProductData(
+                            SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                .setName(item.getProduct().getName())
+                                .build()
+                        )
+                        .build()
+                )
+                .build();
+            builder.addLineItem(lineItem);
+        });
+
+        var session = Session.create(builder.build());
+
+
         cartService.clearCart(cartId);
 
-        return new OrderCheckoutDto(order.getId());
+        return new OrderCheckoutDto(order.getId(), session.getUrl());
     }
 }
