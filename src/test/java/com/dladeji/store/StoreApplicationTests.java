aec721c86d407ef8cd4873e446dd3631597f5223
davidladeji <71533464+davidladeji@users.noreply.github.com>
2025-06-05 12:17:53 -0700

    fix: Update test package name

--- src/test/java/com/dladeji/store/StoreApplicationTests.java
+++ src/test/java/com/dladeji/store/StoreApplicationTests.java
@@ -1,9 +1,7 @@
-package com.codewithmosh.store;
+package com.dladeji.store;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.dladeji.store.StoreApplication;
 
 @SpringBootTest(classes = StoreApplication.class)
 class StoreApplicationTests {
